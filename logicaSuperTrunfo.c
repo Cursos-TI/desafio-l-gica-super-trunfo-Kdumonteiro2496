1fbfb73ce8924ba37c02172f915dae6c8eb8aca1
Carlos Monteiro <55990118+Kdumonteiro2496@users.noreply.github.com>
2026-08-17 21:58:27 -0300

    Remove unused stdio.h include
    Removed unnecessary include directive for stdio.h.

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 // Desafio Super Trunfo - Países
 // Tema 2 - Comparação das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
